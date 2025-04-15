b541aca5bb4569e6dbe4a8053af7b154c521aba8
Guillermo Rojas <apemangr@gmail.com>
2025-04-15 14:58:26 -0400

    Updated to version 17.1.0

--- central/pca10056/s140/ses/Output/Debug/Obj/ble_app_uart_c_pca10056_s140/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/ble_app_uart_c_pca10056_s140/ses_startup_nrf_common.o: \
- C\:\Users\guill\OneDrive\Desktop\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
--- central/pca10056/s140/ses/Output/Debug/Obj/ble_app_uart_c_pca10056_s140/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/ble_app_uart_c_pca10056_s140/thumb_crt0.o: \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/source/thumb_crt0.s
